390301749cf20a71e52e192fe422b5c4e2e0595e
isoppp <prog@d2deck.com>
2021-09-08 23:38:58 +0900

    add ent.Client as a dependency

--- resolver.go
+++ resolver.go
@@ -1,7 +1,19 @@
 package todo
 
+import (
+	"todo/ent"
+
+	"github.com/99designs/gqlgen/graphql"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{ client *ent.Client }
+
+func NewSchema(client *ent.Client) graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		Resolvers: &Resolver{client},
+	})
+}
